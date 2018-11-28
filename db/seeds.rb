05c2ecf19d017f962ce5edd929cbc51320f021a1
geenstijl1 <geenstijl1@hotmail.com>
2018-11-28 12:32:14 -0600

    Four commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,2 @@
-User.create(name: '', email: '', password: '', password_confirmation: '')
+User.create(name: 'secretaria', email: 'angel@hotmail.com', password: 'contra123', password_confirmation: 'contra123')
+User.create(name: 'Israel', email: 'israel@admin.com', password: 'contra123', password_confirmation: 'contra123')
